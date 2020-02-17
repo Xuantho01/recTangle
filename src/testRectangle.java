611b8a2330f2c27d998928df7a2281b620350076
Xuantho01 <hoxuantho95@gmail.com>
2020-02-17 15:48:14 +0700

    test oop

---
+++ src/testRectangle.java
@@ -0,0 +1,7 @@
+public class testRectangle {
+    public static void main(String[] args) {
+        Rectangle rec = new Rectangle(2,2);
+        System.out.println("area: "+rec.getArea());
+        System.out.println("Perimeter: "+rec.getPerimeter());
+    }
+}
